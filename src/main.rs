4d9612101d6330ed307b3adeda64ededbd2f50bd
Pranay Garg <pranaygarg2013@gmail.com>
2022-05-27 20:36:16 -0700

    Add basic tests

--- src/main.rs
+++ src/main.rs
@@ -14,3 +14,18 @@ fn pandu() -> &'static str {
 fn rocket() -> _ {
     rocket::build().mount("/", routes![index, pandu])
 }
+
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::Client;
+    use rocket::http::Status;
+
+    #[test]
+    fn hello_world() {
+        let client = Client::new(rocket()).expect("valid rocket instance");
+        let mut response = client.get("/").dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.body_string(), Some("Hello, world!".into()));
+    }
+}
